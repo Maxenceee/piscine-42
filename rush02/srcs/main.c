12f18ff044c74fcb51fbfc9295496a02d114dcfa
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2022-09-18 12:25:28 +0200

    Update main.c

--- rush02/srcs/main.c
+++ rush02/srcs/main.c
@@ -22,19 +22,25 @@ int	main(int ac, char **av)
 {
 	char			*dict_name;
 	t_parsed_dict	dict;
+	int				number;
 
 	dict_name = "resources/numbers.dict";
 	if (ac == 2)
 	{
 		printf("default dict\n");
+		number = av[1];
 	}
 	else if (ac == 3)
 	{
-		dict_name = av[2];
 		printf("custom dict\n");
+		dict_name = av[1];
+		number = av[2];
 	}
 	else
+	{
 		printf("invalid arguments\n");
+		return (1);
+	}
 
 	printf("number to write %s\n", av[1]);
 	dict = parse_dict(dict_name);
